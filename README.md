b13f65547624549623e245eee04772e5b8333f8a
Hamed Asadi Asl <90451086+hamed3958@users.noreply.github.com>
2021-09-22 15:52:41 +0330

    final design, add readme and screenshot

---
+++ README.md
@@ -0,0 +1,29 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX).
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+
+![](<images/ScreenshotTipCalculatorApp(HAA).png>)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+## Author
+
+- Website - [Hamed Asadi Asl](http://hamedasadiasl.ir/)
+- Frontend Mentor - [@hamed3958](https://www.frontendmentor.io/profile/hamed3958)
+- Github - [@hamed3958](https://github.com/hamed3958)
--- main.js
+++ main.js
@@ -5,16 +5,24 @@ var total = document.querySelector("#total");
 var amount = document.querySelector("#amount");
 
 function tipCounter(value) {
-
   if (bill.value > 0 && people.value > 0) {
     let result = bill.value * value / people.value;
     let totalResult = bill.value * value;
     amount.innerHTML = result.toFixed(2);
     total.innerHTML = totalResult.toFixed(2);
-  } else {
+  } else if (bill.value <= 0) {
     document.querySelector('.error1').style.visibility = 'visible';
+    setTimeout(() => {
+      document.querySelector('.error1').style.visibility = 'hidden';
+    }, 1500);
+    // alert("Bill input can't be zero !")
+  } else if (people.value <= 0) {
     document.querySelector('.error2').style.visibility = 'visible';
-    // alert("inputs can't be zero !")
+    document.querySelector("#people-input").focus();
+    setTimeout(() => {
+      document.querySelector('.error2').style.visibility = 'hidden';
+    }, 1500);
+    // alert("Number of people input can't be zero !")
   }
 }
 
@@ -25,5 +33,5 @@ function reset() {
   amount.innerHTML = "$0.00";
   custom.value = "";
   document.querySelector('.error1').style.visibility = 'hidden';
-    document.querySelector('.error2').style.visibility = 'hidden';
+  document.querySelector('.error2').style.visibility = 'hidden';
 }
